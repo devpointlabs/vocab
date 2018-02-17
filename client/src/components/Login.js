b2098e25f3980c3ff9ba3c080200f89c0721847d
David Jungst <djungst@gmail.com>
2018-02-17 10:07:00 -0700

    fix indentation in login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -22,39 +22,39 @@ class Login extends Component {
     const { email, password } = this.state;
     return (
       <Container>
-         <Grid textAlign='center' style={styles.login_grid}>
-           <Grid.Column style={styles.login_container} mobile={16} tablet={8} computer={8} largeScreen={5} widescreen={5} >
-        <Header as='h1' textAlign='center'>Login</Header>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Field>
-            <label htmlFor='email'>Email</label>
-            <input
-              required
-              id='email'
-              value={email}
-              placeholder='Email'
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Form.Field>
-            <label htmlFor='password'>Password</label>
-            <input
-              required
-              id='password'
-              value={password}
-              placeholder='Password'
-              type='password'
-              onChange={this.handleChange}
-            />
-          </Form.Field>
-          <Segment textAlign='center' basic>
-            <Button primary type='submit'>Submit</Button>
-          </Segment>
-        </Form>
-        </Grid.Column>
+        <Grid textAlign='center' style={styles.login_grid}>
+          <Grid.Column style={styles.login_container} mobile={16} tablet={8} computer={8} largeScreen={5} widescreen={5} >
+            <Header as='h1' textAlign='center'>Login</Header>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Field>
+                <label htmlFor='email'>Email</label>
+                <input
+                  required
+                  id='email'
+                  value={email}
+                  placeholder='Email'
+                  onChange={this.handleChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label htmlFor='password'>Password</label>
+                <input
+                  required
+                  id='password'
+                  value={password}
+                  placeholder='Password'
+                  type='password'
+                  onChange={this.handleChange}
+                />
+              </Form.Field>
+              <Segment textAlign='center' basic>
+                <Button primary type='submit'>Submit</Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
         </Grid>
       </Container>
-    );
+      );
   }
 }
 
